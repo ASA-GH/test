38e3beb6598ff510cd974db7466f401830dfd30c
ASA-GH <samorodov21@gmail.com>
2020-03-02 00:31:42 +0700

    fakerdb

---
+++ server/fakerdb.js
@@ -0,0 +1,40 @@
+module.exports = function(){
+    var faker = require("faker");
+    var lodash = require("lodash");
+    return{
+      items: lodash.times(100, function(n){
+        return{
+            id: n,
+            name:faker.internet.userName(),
+            uniqueUrlName: null,
+            photos: {
+                small:faker.image.avatar(),
+                large: null
+              },
+              status:faker.hacker.phrase(),
+              followed: false
+        }
+      }),
+    options:{
+    totalCount: 100,
+    error: null
+    }
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
