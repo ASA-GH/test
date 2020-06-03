271edeeb60ad7593e79dd72a30c7f90448d66044
ASA-GH <samorodov21@gmail.com>
2020-06-04 00:31:33 +0700

    Update HeaderContainer,  Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,35 @@ import styles from './Header.module.css';
 import headerLogo from '../../assets/images/joker/Joker3.png';
 
 const Header = (props) => {
-    return <header className={styles.header}>
+    return 
+    from 'react';
+    import './App.css';
+    import Navbar from './components/Navbar/Navbar';
+    import Card from './../Widget/Card/Card.jsx';
+    
+    const App = (props) => {
+      return (
+        <Card 
+          className="Header"
+          width = { 1200 }
+          height = { 1200 }
+          Tag = 'header'
+          >
+                    <Navbar />
+                    <Card 
+                    className="appInner"
+                    width = { 90% }
+                    height = { 100% } >
+                    <HeaderContainer />
+                    <Content />
+                    <FooterContainer /> 
+                    </Card>
+                </Card>
+      )
+    }
+    
+    export default App;
+    (<header className={styles.header}>
        <span>
            <img src={headerLogo} />
         </span>
@@ -12,6 +40,7 @@ const Header = (props) => {
         <div className={styles.b}>You don’t like to communicate? You are welcome!</div>
 </span>
     </header>
+    )
 }
 
 export default Header;
