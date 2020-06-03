862617e43fc53551c9bd5b225d2d4e5003629e5d
ASA-GH <samorodov21@gmail.com>
2020-06-03 23:51:06 +0700

    Update App.js Card

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
 import FooterContainer from './components/Footer/FooterContainer';
+import Card from './components/Widget/Card/Card.jsx';
 
-const App = () => {
+const App = (props) => {
     return (
-            <div className='app-wrapper'>
-                <HeaderContainer />
+            <Card 
+            className="appWrapper"
+            width = { 1200 }
+            height = { 1200 }
+            >
                 <Navbar />
+                <Card 
+                className="appInner"
+                width = { 90% }
+                height = { 100% } >
+                <HeaderContainer />
                 <Content />
-                <FooterContainer />
-            </div>
+                <FooterContainer /> 
+                </Card>
+            </Card>
         )
 }
 
