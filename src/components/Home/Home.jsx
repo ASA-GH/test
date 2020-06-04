1ae669cba0733491548caeab0c2b4f5479b260af
ASA-GH <samorodov21@gmail.com>
2020-06-05 00:12:07 +0700

    Updata Home, Add HomeContainer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-lmport './Home.css';
-import Header from './../Header/HeaderContainer.js'
+import './Home.css';
+/*import Header from './../Header/HeaderContainer.js'
 import Profile from './components/Profile/Profile';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import News from './components/News/News';
-
-const App = () => {
-  return (
+*/
+const Home = () => {
+  /*return (
     <div className='app-wrapper'>
                 <Header />
                 <Navbar />
        
             </div>
-  )
+  )*/
 }
 
-export default App;
+export default Home;
---
+++ src/components/Home/HomeContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './Home'
+
+const HomeContainer = (props) => {
+  return (
+    <Home />
+  )
+}
+
+export default HomeContainer;
