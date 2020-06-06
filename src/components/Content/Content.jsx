cc2b81cab1e429592e80eca6bea2b16e9c99b761
ASA-GH <samorodov21@gmail.com>
2020-06-06 23:55:48 +0700

    Update Content.jsx

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,13 +6,25 @@ import UsersContainer from './../Users/UsersContainer';
 import NewsContainer from './../News/NewsContainer';
 import HomeContainer from './../Home/HomeContainer';
 
-const  Content = (props) => (
+const  Content = (props) => {
+ return(
+    <div style={{ backgroundColor: '#cc0c2f', width: '90%',
+    height: '100%'}} />
+
+)}
+
+   /*    
+ (
 <Fragment>
     <Route path='/dialogs' render={ () => <DialogsContainer /> }/>
     <Route path='/profile' render={ () => <ProfileContainer /> }/>
     <Route path='/users' render={ () => <UsersContainer /> }/>
     <Route path='/news' render={ () => <NewsContainer /> }/>
     <Route path='/home' render={ () => <HomeContainer /> }/>
-</Fragment>               
-    );
+</Fragment>    
+          
+
+);*/
+
+
     export default Content;
