1e835ea870ecfdb9eb6878008e81272e8cb93d26
ASA-GH <samorodov21@gmail.com>
2020-06-17 23:17:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 /* eslint-disable */
 import React from 'react';
 import './App.css';
-import HeaderContainer from './components/Header/HeaderContainer';
+import FooterContainer from './components/HeaderAndFooter/FooterContainer';
+import HeaderContainer from './components/HeaderAndFooter/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
-import FooterContainer from './components/Footer/FooterContainer';
+import HomeContainer from './components/Home/HomeContainer';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import SettingsContainer from './components/Settings/SettingsContainer';
+import NewsContainer from './components/News/NewsContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import MusicContainer from './components/Music/MusicContainer';
+
+
+
+
+
 import Card from './components/Widget/Card/Card';
 const App = (props) => {
     
@@ -14,8 +26,15 @@ const App = (props) => {
           <Navbar />
           <Card innerApp >
            <HeaderContainer />
-            <Content />
-           <FooterContainer />
+         {/*  <Content /> 
+           <HomeContainer/>
+           <DialogsContainer/>*/}
+          <MusicContainer />
+     {/*     <ProfileContainer/>
+            <NewsContainer/>
+       <UsersContainer/>
+            <SettingsContainer/>*/}
+        <FooterContainer />
           </Card> 
         </Card>   
       </div>
